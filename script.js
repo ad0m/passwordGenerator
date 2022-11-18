e750261a821a0ac5c5d732fc40868e6c950b9ef7
ad0m <adamweldon@gmail.com>
2022-11-18 19:51:41 +0000

    U:passLength var to 0 an event listener to top

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // Array of special characters to be included in password
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+
 var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var passLength = 0;
 
 // Function to prompt user for password options
 function getPasswordOptions() {
@@ -33,9 +40,6 @@ function generatePassword() {
   passLength = (prompt('How long would you like you password to be? Please enter a number between 10 & 64.'));
   getPasswordOptions();
 }
-
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
